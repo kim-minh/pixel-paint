73f03b3c8188794888ee12c7b7926255d7def8a9
kim-minh <100175752+kim-minh@users.noreply.github.com>
2022-07-08 16:26:35 +0700

    Add cutom grid size

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const container = document.querySelector('.container');
+const sizeButton = document.querySelector('.size');
 
-const rows = 16, cols = 16;
-const size = rows * cols;
-for(let i = 0; i < size; ++i) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    container.appendChild(cell);
+//Create a grid of n x n
+function createGrid(size) {
+    for(let i = 0; i < size * size; ++i) {
+        //Create new cell
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        container.appendChild(cell);
+    }
+    container.style.setProperty('--size', size); //Set custom property for css
 }
+
+//Create default grid of 16 x 16
+createGrid(16);
+
+//Add event for customize button
+sizeButton.addEventListener('click', () => {
+    const size = prompt('Choose grid size', getComputedStyle(container).getPropertyValue('--size'));
+    if(size != undefined) {
+        container.replaceChildren();
+        createGrid(size);
+    }
+});
